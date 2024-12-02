f0686621fc5b0e683aeba2048f0ae7077dc17ed5
MoHamed1hp <145708151+MoHamed1hp@users.noreply.github.com>
2024-12-02 21:32:39 +0200

    LOL
    books html
    7agat fe el register
    ma4 faker eh kaman

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+$username = $_POST['username'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+$userType = `user`
+
+// $connection = mysqli_connect(hostname: 'sana', username: 'sana', password: 'sana', database: 'sana');
+// mysqli_query($connection, "INSERT INTO USER(`USERNAME`, `EMAIL`, `PASSWORD`, `USERTYPE`) VALUES('$username', '$email', '$password',`$userType`)");
+
+?>
