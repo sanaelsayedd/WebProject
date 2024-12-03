d849932bc003605cb94b4a74670b602422453e3f
MoHamed1hp <145708151+MoHamed1hp@users.noreply.github.com>
2024-12-03 03:26:14 +0200

    7agat 5afefa

---
+++ register.php
@@ -0,0 +1,16 @@
+<?php
+if(isset($_POST["login"])){
+    $username = htmlspecialchars($_POST['username'], ENT_QUOTES, 'UTF-8');
+    $email = htmlspecialchars($_POST['email'], ENT_QUOTES, 'UTF-8');
+    $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+    $userType = 'user';
+
+   
+// $connection = mysqli_connect(hostname: 'sana', username: 'sana', password: 'sana', database: 'sana');
+// mysqli_query($connection, "INSERT INTO USER(`USERNAME`, `EMAIL`, `PASSWORD`, `USERTYPE`) VALUES('$username', '$email', '$password',`$userType`)");
+
+
+}
+
+
+?>
